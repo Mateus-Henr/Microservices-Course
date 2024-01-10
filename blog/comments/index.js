b5182cfa38c6721240288cede090b53d521debbd
Matt <mateush.study@gmail.com>
2024-01-10 13:48:10 -0300

    Comments and posts codes

---
+++ blog/comments/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const {randomBytes} = require('crypto');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) =>
+{
+    res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) =>
+{
+    const commentId = randomBytes(4).toString('hex');
+    const comments = commentsByPostId[req.params.id] || [];
+
+    comments.push({
+        id: commentId,
+        title: req.body.content
+    });
+
+    commentsByPostId[req.params.id] = comments;
+
+    res.status(201).send(commentsByPostId[req.params.id]);
+});
+
+app.listen(4001, () => console.log('Listening on 4000'));
