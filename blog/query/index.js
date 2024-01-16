06122466a90b6d0ddc09ffe449177e4c75947b1e
Matt <mateush.study@gmail.com>
2024-01-16 11:11:14 -0300

    Finish section 2 - Handling missing events

--- blog/query/index.js
+++ blog/query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,15 +9,8 @@ app.use(cors());
 
 const posts = {}
 
-app.get('/posts', (req, res) =>
+const handleEvent = (type, data) =>
 {
-    res.send(posts);
-});
-
-app.post('/events', (req, res) =>
-{
-    const {type, data} = req.body;
-
     if (type === 'PostCreated')
     {
         const {id, title} = data;
@@ -47,8 +41,37 @@ app.post('/events', (req, res) =>
 
         posts[postId].comments[commentIdx] = data;
     }
+}
+
+app.get('/posts', (req, res) =>
+{
+    res.send(posts);
+});
+
+app.post('/events', (req, res) =>
+{
+    const {type, data} = req.body;
+
+    handleEvent(type, data);
 
     res.send({status: 'OK'});
 });
 
-app.listen(4002, () => console.log('Listening on 4002'));
+app.listen(4002, async () =>
+{
+    console.log("Listening on 4002");
+    try
+    {
+        const res = await axios.get("http://localhost:4005/events");
+
+        for (let event of res.data)
+        {
+            console.log("Processing event:", event.type);
+
+            handleEvent(event.type, event.data);
+        }
+    } catch (error)
+    {
+        console.log(error.message);
+    }
+});
