784bd94305ab8b791c8b4c0db5cb19efabeb53c3
Matt <mateush.study@gmail.com>
2024-01-24 15:37:46 -0300

    Fix bugs

--- blog/client/src/CommentList.js
+++ blog/client/src/CommentList.js
@@ -1,29 +1,24 @@
 import React from "react";
 
-const CommentList = ({comments}) =>
-{
-    const renderedComments = comments.map((comment) =>
-    {
-        let content;
+const CommentList = ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    let content;
 
-        if (comment.status === 'approved')
-        {
-            content = comment.content;
-        }
-        else if (comment.status === 'pending')
-        {
-            content = 'This comment is awaiting moderation';
-        }
-        else if (comment.status === 'rejected')
-        {
-            content = 'This comment has been rejected';
-        }
+    if (comment.status === "approved") {
+      content = comment.content;
+    }
 
+    if (comment.status === "pending") {
+      content = "This comment is awaiting moderation";
+    }
 
-        return <li key={comment.id}>{content}</li>;
-    });
+    if (comment.status === "rejected") {
+      content = "This comment has been rejected";
+    }
 
-    return <ul>{renderedComments}</ul>;
+    return <li key={comment.id}>{content}</li>;
+  });
+
+  return <ul>{renderedComments}</ul>;
 };
-
 export default CommentList;
