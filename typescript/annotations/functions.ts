8b6e189283d78c29086ee5c39f78e2c80b758b8e
Matt <mateush.study@gmail.com>
2024-01-25 10:58:42 -0300

    More on typescript basics (functions)

--- typescript/annotations/functions.ts
+++ typescript/annotations/functions.ts
@@ -9,3 +9,40 @@ const add = (a: number, b: number): number => {
 const subtract = (a: number, b: number) => {
     a - b;
 };
+
+
+// Anonymous function example
+const multiply = function divide(a: number, b: number): number {
+    return a * b;
+};
+
+
+// No return value.
+const logger = (message: string): void => {
+    console.log(message);
+};
+
+// The function is never going to reach its end.
+const throwError = (message: string): never => {
+    throw new Error(message);
+};
+
+// This example we still expect it to get the end.
+const throwError2 = (message: string): void => {
+    if (!message) {
+        throw new Error(message);
+    }
+};
+
+
+// Destructuring with annotations
+const forecast = {
+    date: new Date(),
+    weather: 'sunny'
+}
+
+// Destructuring and annotations are apart.
+const logWeather = ({date, weather}: { date: Date, weather: string }) => {
+    console.log(date);
+    console.log(weather);
+};
