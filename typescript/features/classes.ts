20a87b121520583f87490dc12c1456f2e8c44aaf
Matt <mateush.study@gmail.com>
2024-01-26 10:31:50 -0300

    More on typescript basics (Classes)

---
+++ typescript/features/classes.ts
@@ -0,0 +1,40 @@
+class Vehicle {
+
+    constructor(public color: string) { // Defining a field.
+    }
+
+
+    public drive(): void {
+        console.log('Driving');
+    }
+
+    protected honk(): void {
+        console.log('Beep');
+    }
+}
+
+class Car extends Vehicle {
+    constructor(public wheels: number, color: string) {
+        super(color);
+    }
+
+    public drive(): void { // Overriding method.
+        console.log('Vroom');
+        this.internalStuff();
+    }
+
+    private internalStuff(): void {
+        this.honk();
+    }
+}
+
+// Defining a method as private doesn't mean more security, it just means that other developers cannot use that method.
+// Protected means that we can access the method in child classes as well.
+const vehicle = new Vehicle('orange');
+vehicle.drive();
+
+
+const car = new Car(4, 'orange');
+car.drive();
+
+console.log(vehicle.color);
