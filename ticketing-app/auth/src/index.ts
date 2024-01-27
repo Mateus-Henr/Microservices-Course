ca97c626d73dea59542adde8afa2005b057df85b
Matt <mateush.study@gmail.com>
2024-01-27 19:38:14 -0300

    Ticketing app - adding routers

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express';
 import {json} from 'body-parser';
+import {currentUserRouter} from "./routes/current-user";
+import {signInRouter} from "./routes/signin";
+import {signUpRouter} from "./routes/signup";
+import {signOutRouter} from "./routes/signout";
 
 const app = express();
 app.use(json());
-
-app.get('/api/users/currentuser', (req, res) => {
-    res.send("Worked!");
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signUpRouter);
+app.use(signOutRouter);
 
 app.listen(3000, () => {
     console.log("Running on port 3000.");
