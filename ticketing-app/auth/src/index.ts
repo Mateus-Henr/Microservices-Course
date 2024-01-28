896a87842dbed656d29d9c430cfe7b6273e4d2b2
Matt <mateush.study@gmail.com>
2024-01-27 21:36:36 -0300

    Ticketing app - adding error handling

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -4,13 +4,17 @@ import {currentUserRouter} from "./routes/current-user";
 import {signInRouter} from "./routes/signin";
 import {signUpRouter} from "./routes/signup";
 import {signOutRouter} from "./routes/signout";
+import {errorHandler} from "./middlewares/error-handlers";
 
 const app = express();
 app.use(json());
+
 app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signUpRouter);
 app.use(signOutRouter);
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
     console.log("Running on port 3000.");
