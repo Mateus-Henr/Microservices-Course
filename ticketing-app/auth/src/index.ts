188fb22bb06d77197df45c35c7bc5a83ad828010
Matt <mateush.study@gmail.com>
2024-02-04 10:39:32 -0300

    Initial test logic

--- ticketing-app/auth/src/index.ts
+++ ticketing-app/auth/src/index.ts
@@ -1,36 +1,5 @@
-import express from 'express';
-
-require('express-async-errors');
-import {json} from 'body-parser';
-import {currentUserRouter} from "./routes/current-user";
-import {signInRouter} from "./routes/signin";
-import {signUpRouter} from "./routes/signup";
-import {signOutRouter} from "./routes/signout";
-import {errorHandler} from "./middlewares/error-handlers";
-import {NotFoundError} from "./errors/not-found-error";
-import cookieSession from "cookie-session";
-import mongoose from 'mongoose';
-
-const app = express();
-
-app.set('trust proxy', true); // Secure traffic
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true // Https connection
-    })
-)
-
-app.use(currentUserRouter);
-app.use(signInRouter);
-app.use(signUpRouter);
-app.use(signOutRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-app.use(errorHandler);
+import mongoose from "mongoose";
+import {app} from "./app";
 
 const start = async () => {
     try {
