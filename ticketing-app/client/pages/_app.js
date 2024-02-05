079b5e81bc53811ac3d1e4f28bd6fdd42a4e6e77
Matt <mateush.study@gmail.com>
2024-02-05 14:39:43 -0300

    Header and signin page

--- ticketing-app/client/pages/_app.js
+++ ticketing-app/client/pages/_app.js
@@ -1,7 +1,36 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from "./api/build-client";
+import Header from "../components/header";
 
 // All components are going to pass through this component. That's why global CSS is imported here.
-export default ({Component, pageProps}) =>
+const AppComponent = ({Component, pageProps, currentUser}) =>
 {
-    return <Component {...pageProps} />
+    return (
+        <div>
+            <Header currentUser={currentUser} />
+            <Component {...pageProps} />
+        </div>
+    );
 };
+
+// Note that the arguments in the 'context' object is different when handling a Custom app component in comparison to a
+// page.
+AppComponent.getInitialProps = async (appContext) =>
+{
+    const client = buildClient(appContext.ctx);
+    const {data} = await client.get('/api/users/currentuser');
+
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) // For the pages that don't have the function implemented.
+    {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+
+
+    return {
+        pageProps,
+        ...data
+    };
+}; // When adding this function to the app component, this same function doesn't work automatically on pages anymore.
+
+export default AppComponent;
--- ticketing-app/client/pages/index.js
+++ ticketing-app/client/pages/index.js
@@ -7,7 +7,7 @@ const LandingPage = ({currentUser}) =>
     console.log(currentUser);
     // Making a request from the component.
     // axios.get('/api/users/currentuser');
-    return <h1 style={{color: 'red'}}>Landing Page</h1>;
+    return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
 // Automatically called when the server renders the component. Used to fetch data.
@@ -15,12 +15,12 @@ const LandingPage = ({currentUser}) =>
 // We cannot fetch data from inside a component. Hooks can only be used inside components.
 // This function is executed on the server, however when the user is navigating from one page to another while in the
 // app, it is executed on the client.
-LandingPage.getInitialProps = async (context) =>
-{
-    const client = buildClient(context);
-    const {data} = await client.get('/api/users/currentuser');
-
-    return data;
-};
+// LandingPage.getInitialProps = async (context) =>
+// {
+//     const client = buildClient(context);
+//     const {data} = await client.get('/api/users/currentuser');
+//
+//     return data;
+// };
 
 export default LandingPage;
