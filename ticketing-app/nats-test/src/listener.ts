6449ca420d8fc605ea6432f9a1e65eb97423aa0c
Matt <mateush.study@gmail.com>
2024-02-26 12:05:00 -0300

    Code refactoring

--- ticketing-app/nats-test/src/listener.ts
+++ ticketing-app/nats-test/src/listener.ts
@@ -1,5 +1,6 @@
 import nats, {Message, Stan} from 'node-nats-streaming';
 import {randomBytes} from 'node:crypto';
+import {TickedCreatedListener} from './events/ticket-created-listener';
 
 console.clear();
 
@@ -22,63 +23,3 @@ stan.on('connect', () => {
 // Closing the connection whenever interruption signals are signed.
 process.on('SIGINT', () => stan.close());
 process.on('SIGTERM', () => stan.close());
-
-abstract class Listener {
-    abstract subject: string;
-    abstract queueGroupName: string;
-
-    protected abstract onMessage(data: any, msg: Message): void;
-
-    private client: Stan;
-    protected ackWait = 5 * 1000; // 5s
-
-    constructor(client: Stan) {
-        this.client = client;
-    }
-
-    private subscriptionOptions() {
-        return this.client
-            .subscriptionOptions()
-            .setDeliverAllAvailable()
-            .setManualAckMode(true)
-            .setAckWait(this.ackWait)
-            .setDurableName(this.queueGroupName);
-    }
-
-    listen() {
-        const subscription = this.client.subscribe(
-            this.subject,
-            this.queueGroupName,
-            this.subscriptionOptions()
-        );
-
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message received: ${this.subject} / ${this.queueGroupName}`
-            );
-
-            const parsedData = this.parseMessage(msg);
-            this.onMessage(parsedData, msg);
-        });
-    }
-
-    private parseMessage(msg: Message) {
-        const data = msg.getData();
-
-        return typeof data === 'string'
-            ? JSON.parse(data)
-            : JSON.parse(data.toString('utf8'));
-    }
-}
-
-class TickedCreatedListener extends Listener {
-    subject = 'ticket:created';
-    queueGroupName = 'payments-service';
-
-    onMessage(data: any, msg: Message): void {
-        console.log('Event data!', data);
-
-        msg.ack();
-    }
-
-}
