35d5806fda09ebfbe1874fce9eaad8d968477cdb
Matt <mateush.study@gmail.com>
2024-02-29 16:16:25 -0300

    Reference fix

--- ticketing-app/nats-test/src/events/ticket-created-publisher.ts
+++ ticketing-app/nats-test/src/events/ticket-created-publisher.ts
@@ -2,5 +2,5 @@ import {Publisher, Subjects, TicketCreatedEvent} from "@sgtickers-course/common"
 
 
 export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
-    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    readonly subject: Subjects.TicketCreated = Subjects.TicketCreated;
 }
