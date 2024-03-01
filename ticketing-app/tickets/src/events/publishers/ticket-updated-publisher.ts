7901eca87cbde970918d9d6a8f5691173cd9ba54
Matt <mateush.study@gmail.com>
2024-02-29 21:34:56 -0300

    Update on tests

---
+++ ticketing-app/tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import {Publisher, Subjects, TicketUpdatedEvent} from '@sgtickers-course/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+    subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
