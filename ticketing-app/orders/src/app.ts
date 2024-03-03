0efc7759a7ef0b0623d2ea1cd353cf902745ed3e
matt <mateush.study@gmail.com>
2024-03-02 23:59:24 -0300

    Orders service setup

--- ticketing-app/orders/src/app.ts
+++ ticketing-app/orders/src/app.ts
@@ -5,6 +5,10 @@ import {json} from 'body-parser';
 import {currentUser, errorHandler} from '@sgtickers-course/common';
 import {NotFoundError} from '@sgtickers-course/common';
 import cookieSession from 'cookie-session';
+import {deleteOrderRouter} from './routes/delete';
+import {showOrderRouter} from './routes/show';
+import {newOrderRouter} from './routes/new';
+import {indexOrderRouter} from "./routes";
 
 const app = express();
 
@@ -17,10 +21,10 @@ app.use(
     })
 )
 app.use(currentUser);
-app.use(createTicketRouter);
-app.use(showTicketRouter);
-app.use(indexTicketRouter);
-app.use(updateTicketRouter);
+app.use(deleteOrderRouter);
+app.use(showOrderRouter);
+app.use(newOrderRouter);
+app.use(indexOrderRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError();
