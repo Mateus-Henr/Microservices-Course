1673fcf716a4f7eb5b40a4521692a50c9c1f4af8
matt <mateush.study@gmail.com>
2024-05-16 20:55:38 -0300

    Order tests

--- ticketing-app/orders/src/routes/delete.ts
+++ ticketing-app/orders/src/routes/delete.ts
@@ -1,10 +1,27 @@
 import express, {Request, Response} from "express";
+import {Order} from "../models/orders";
+import {NotAuthorizedError, NotFoundError, OrderStatus} from "@sgtickers-course/test";
 
 const router = express.Router();
 
 router.delete("/api/orders/:orderId", async (req: Request, res: Response) =>
 {
-    res.send({});
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order)
+    {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id)
+    {
+        throw new NotAuthorizedError();
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.status(204).send(order);
 });
 
 export {router as deleteOrderRouter};
--- ticketing-app/orders/src/routes/show.ts
+++ ticketing-app/orders/src/routes/show.ts
@@ -1,10 +1,24 @@
 import express, {Request, Response} from "express";
+import {Order} from "../models/orders";
+import {NotAuthorizedError, NotFoundError} from "@sgtickers-course/test";
 
 const router = express.Router();
 
 router.get("/api/orders/:orderId", async (req: Request, res: Response) =>
 {
-    res.send({});
+    const order = await Order.findById(req.params.orderId).populate("ticket");
+
+    if (!order)
+    {
+        throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id)
+    {
+        throw new NotAuthorizedError();
+    }
+
+    res.send(order);
 });
 
 export {router as showOrderRouter};
