061875a110385684ffeccce90bc733878a7f435e
matt <mateush.study@gmail.com>
2024-05-16 15:29:29 -0300

    New order tests

---
+++ ticketing-app/orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+import {app} from "../../app";
+import request from "supertest";
+import {Ticket} from "../../models/ticket";
+import {OrderStatus} from "@sgtickers-course/test";
+import {Order} from "../../models/orders";
+
+it("returns an error if the ticket does not exist", async () =>
+{
+    const ticketId = new mongoose.Types.ObjectId();
+
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.signin())
+        .send({ticketId})
+        .expect(404);
+});
+
+it("returns an error if the ticket is already reserved", async () =>
+{
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 20
+    });
+
+    await ticket.save();
+
+    const order = Order.build({
+        ticket,
+        userId: "fsfdsfds",
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+
+    await order.save();
+
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.signin())
+        .send({ticketId: ticket.id})
+        .expect(400);
+});
+
+it("reserves a ticket", async () =>
+{
+    const ticket = Ticket.build({
+        title: "concert",
+        price: 20
+    });
+
+    await ticket.save();
+
+    await request(app)
+        .post("/api/orders")
+        .set("Cookie", global.signin())
+        .send({ticketId: ticket.id})
+        .expect(201);
+});
