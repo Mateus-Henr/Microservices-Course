1fa3eb9008a9cf5e5f75554b1aaf003fc4252564
matt <mateush.study@gmail.com>
2024-05-20 14:06:35 -0300

    Order listener

--- ticketing-app/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing-app/orders/src/events/listeners/ticket-created-listener.ts
@@ -10,10 +10,10 @@ export class TicketCreatedListener extends Listener<TicketCreatedEvent>
 
     async onMessage(data: TicketCreatedEvent["data"], msg: Message)
     {
-        const {title, price} = data;
+        const {id, title, price} = data;
 
         const ticket = Ticket.build({
-            title, price
+            id, title, price
         });
 
         await ticket.save();
