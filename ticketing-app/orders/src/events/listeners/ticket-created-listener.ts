80baeff4cd5a0f0f47500b5469f3a61a3636d0a7
matt <mateush.study@gmail.com>
2024-05-20 13:57:34 -0300

    Order listener

---
+++ ticketing-app/orders/src/events/listeners/ticket-created-listener.ts
@@ -0,0 +1,24 @@
+import {Listener, Subjects, TicketCreatedEvent} from "@sgtickers-course/test";
+import {Message} from "node-nats-streaming";
+import {queueGroupName} from "./queue-group-name";
+import {Ticket} from "../../models/ticket";
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent>
+{
+    subject: Subjects.TicketCreated = Subjects.TicketCreated;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: TicketCreatedEvent["data"], msg: Message)
+    {
+        const {title, price} = data;
+
+        const ticket = Ticket.build({
+            title, price
+        });
+
+        await ticket.save();
+
+        msg.ack();
+    }
+
+}
